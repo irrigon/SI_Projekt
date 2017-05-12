83a292136298978464640cecf35a1e80074a6fbc
irrigon <ku.irrigon@gmail.com>
2017-05-12 13:43:04 +0200

    ver 0.1
    Uczenie i tworzenie słów

---
+++ SI_Projekt/Node.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI_Projekt{
+    class Node {
+        public Node(char letter) {
+            this.counter = 0;
+            this.total = 0;
+            this.letter = letter;
+            this.children = new Node[27];
+        }
+
+        public string generateNewWord() {
+            string result = "";
+            generateAnotherLetter(ref result);
+            return result;
+        }
+
+        private void generateAnotherLetter(ref string result) {
+            Random rand = new Random();
+            while (true){
+                int index = rand.Next(0, 26);
+                if ((double)children[index].counter / (double)children[index].total > 0.015) {
+                    result += children[index].letter;
+                    if (children[index].letter == '\0' || result.Length > 20) return;
+                    children[index].generateAnotherLetter(ref result);
+                }
+            }
+        }
+
+        public void createGraph(int depth) {
+            createFirstLevel();
+            createNextLevels(depth - 1);
+        }
+
+        public void teach() {
+            string[] words = System.IO.File.ReadAllLines(@"C:\Users\Klient\Documents\Visual Studio 2015\Projects\SI_Projekt\SI_Projekt\WordList.txt");
+            foreach (string word in words)
+                teachNewWord(word,0);
+        }
+
+        private void teachNewWord(string word, int letterPointer) {
+            foreach (Node child in children)
+                child.total++;
+            if (word.Length == letterPointer) {
+                children[26].counter++;
+                return;
+            }
+            if(word[letterPointer] >= 'A' && word[letterPointer] <= 'Z')
+                children[(int)word[letterPointer] - 0x41].counter++;
+            else if (word[letterPointer] >= 'a' && word[letterPointer] <= 'z')
+                children[(int)word[letterPointer] - 0x61].counter++;
+            children[0].teachNewWord(word, letterPointer + 1);
+        }
+
+        private void createFirstLevel() {
+            this.children = createNewLevel();
+        }
+
+        private void createNextLevels(int depth) {
+            if (depth == 0) return;
+            Node[] children = createNewLevel();
+            for (int i = 0; i < 27; i++) 
+                this.children[i].children = children;
+            this.children[0].createNextLevels(depth - 1);
+        }
+        
+        private Node[] createNewLevel() {
+            Node[] children = new Node[27];
+            for (int i = 0; i < 27; i++) {
+                if (i == 26)
+                    children[i] = new Node('\0');
+                else
+                    children[i] = new Node((char)(0x61 + i));
+                if (i != 0)
+                    children[i].brother = children[i - 1];
+            }
+            return children;
+        }
+
+
+        private char letter { get; }
+        private int total { get; set; }
+        private int counter { get; set; }
+        private Node[] children { get; set; }
+        private Node brother { get; set; }
+    }
+}
---
+++ SI_Projekt/Nodev2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SI_Projekt
+{
+    class Nodev2
+    {
+
+        public Nodev2(char letter) {
+            this.letter = letter;
+            this.total = 0;
+            this.counter = 0;
+            this.children = new Nodev2[27];
+        }
+
+        public void createGraph() {
+            this.children = createChildren();
+            foreach (Nodev2 child in this.children)
+                child.children = createChildren();
+        }
+
+        public void teach() {
+            string[] words = System.IO.File.ReadAllLines(
+                @"C:\Users\Klient\Documents\Visual Studio 2015\Projects\SI_Projekt\SI_Projekt\WordList.txt");
+            foreach (string word in words)
+                teachNewWord(word, 0);
+        }
+
+        public string generateNewWord() {
+            string result = "";
+            Random rand = new Random();
+            int index, lastLetter = 0;
+            int lengthOfWord = rand.Next(3, 12);
+            int missesCounter = 0;
+            while (true) {
+                index = rand.Next(0, 25);
+                if (result != "") {
+                    if ((double)children[lastLetter].children[index].counter / children[lastLetter].counter > 0.06)
+                        result += children[lastLetter].children[index].letter;
+                    else {
+                        if(++missesCounter > 50) return result;
+                        continue;
+                    }
+                }
+                else
+                    result += children[index].letter;
+                missesCounter = 0;
+                lastLetter = index;
+                if (result.Length == lengthOfWord)
+                    return result;
+            }
+        }
+
+        private void teachNewWord(string word, int letterPointer) {
+            int lastLetter = 0;
+            while(word.Length > letterPointer) {
+                for (int i = 0; i < 27; i++)
+                    this.children[i].total++;
+
+                if (letterPointer != 0){
+                    for (int i = 0; i < 27; i++)
+                        this.children[lastLetter].children[i].total++;
+                    if (word[letterPointer] >= 'A' && word[letterPointer] <= 'Z')
+                        this.children[lastLetter].children[word[letterPointer] - 0x41].counter++;
+                    else if (word[letterPointer] >= 'a' && word[letterPointer] <= 'z')
+                        this.children[lastLetter].children[word[letterPointer] - 0x61].counter++;
+                }
+
+                if (word[letterPointer] >= 'A' && word[letterPointer] <= 'Z')
+                    lastLetter = word[letterPointer] - 0x41;
+                else if (word[letterPointer] >= 'a' && word[letterPointer] <= 'z')
+                    lastLetter = word[letterPointer] - 0x61;
+
+                this.children[lastLetter].counter++;
+
+                letterPointer++;
+            }
+
+            for (int i = 0; i < 27; i++){
+                this.children[i].total++;
+                this.children[lastLetter].children[i].total++;
+            }
+
+            this.children[26].counter++;
+            this.children[lastLetter].children[26].counter++;
+        }
+
+        private Nodev2[] createChildren() {
+            Nodev2[] children = new Nodev2[27];
+            for (int i = 0; i < 27; i++) {
+                if (i == 26)
+                    children[i] = new Nodev2('\0');
+                else
+                    children[i] = new Nodev2((char)(i + 0x61));
+            }
+            return children;
+        }
+
+        private char letter { get; }
+        private int total { get; set; }
+        private int counter { get; set; }
+        private Nodev2[] children { get; set; }
+    }
+}
