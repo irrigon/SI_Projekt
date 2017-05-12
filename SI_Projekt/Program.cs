c25acb9721686603bb2ffdece16c1c2b78b29c32
irrigon <ku.irrigon@gmail.com>
2017-05-12 13:43:04 +0200

    ver 0.1
    Uczenie i tworzenie słów

---
+++ SI_Projekt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI_Projekt{
+    class Program{
+        static void Main(string[] args){
+            Nodev2 root = new Nodev2(' ');
+            root.createGraph();
+            root.teach();
+            string[] results = new string[100];
+            for (int i = 0; i < 100; i++){
+                results[i] = root.generateNewWord();
+                Console.WriteLine("{0}", results[i]);
+            }
+            return;
+        }
+    }
+}
